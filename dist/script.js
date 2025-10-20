927b9e430aa72b678c52fef5134dcd3429dea9a5
arparvezz <arparvez50@gmail.com>
2025-10-20 12:13:25 +0600

    Dashboard updated

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,13 @@
 let sidebarToggleBtn = document.querySelector(".sidebar-toggle");
-let sidebar = document.querySelector("aside");
+let sidebar = document.querySelector(".sidebar");
+let sidebarOverlay = document.querySelector(".sidebar-overlay");
 
 sidebarToggleBtn.addEventListener("click",function(e){
     e.preventDefault();
     sidebar.style.display = 'unset';
 });
 
-sidebar.addEventListener("click",function(e){
+sidebarOverlay.addEventListener("click",function(e){
     e.preventDefault();
     sidebar.style.display = 'none';
 });
